b393210366f246782e72538b897a53aab1518fe6
Matt Carmody <matt.carmody@ovsgroup.com>
2020-05-01 04:58:40 -0400

    add initial loader

--- src/pages/homepage/homepage.component.jsx
+++ src/pages/homepage/homepage.component.jsx
@@ -4,26 +4,29 @@ import Data from '../../fixtures/data.json';
 import ClassSelector from '../../components/class-selector/class-selector.component';
 import MapContainer from '../../components/map-container/map-container.component';
 import RankContainer from '../../components/rank-container/rank-container.component';
+import logo from '../../fixtures/FFXIV_icon_centered.png';
 import './homepage.styles.scss';
 
 const Homepage = ({ currentJob }) => {
   return (
     <div className="container">
       <ClassSelector options={Object.keys(Data)} />
+      { currentJob ? (
       <div className="row">
         <div className="map-section">
           <MapContainer />
         </div>
         <div className="rank-section">
-          { currentJob ? (
             <RankContainer
               rankNames={Object.keys(Data[currentJob])}
               rankData={Data[currentJob]}
               job={currentJob}
             />
-          ) : null }
         </div>
       </div>
+      ) : <div className="no-selection">
+            <img className='logo' src={logo} alt="FFXIV Icon"/>
+          </div> }
     </div>
   );
 }
